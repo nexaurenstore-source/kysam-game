c827284b65a2a91e1f16ff2bbc22adfa2f28e27f
nexaurenstore-source <nexaurenstore@gmail.com>
2026-08-30 02:28:45 +0200

    feat: add Kysam character profile

---
+++ game/scripts/kysam_profile.gd
@@ -0,0 +1,13 @@
+extends CharacterProfile
+
+func _init() -> void:
+	character_name = "Kysam"
+	max_health = 100.0
+	max_energy = 100.0
+	attack_power = 12.0
+	defense_power = 8.0
+	move_speed = 5.0
+	weapon_name = "Arma de Kysam"
+	weapon_description = "Arma especial ligada ao estilo de combate de Kysam."
+	unique_power_name = "Poder de Kysam"
+	unique_power_description = "Habilidade única de Kysam, a ser definida e implementada no sistema de poderes."
